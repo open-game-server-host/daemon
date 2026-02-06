ab3b7ac70e2effdc46342f388d2d1ac9b5bdf6b9
Dom <dominicraebeeson@gmail.com>
2026-02-06 00:47:23 +0000

    remove load testing

--- src/daemon.ts
+++ src/daemon.ts
@@ -1,4 +1,4 @@
-import { getApps, Logger } from "@open-game-server-host/backend-lib";
+import { Logger } from "@open-game-server-host/backend-lib";
 const logger = new Logger("MAIN");
 logger.info("Starting");
 
@@ -6,7 +6,6 @@ import { existsSync, mkdirSync } from "node:fs";
 import { cleanupPartiallyDownloadedAppArchives } from "./apps/appArchiveCache";
 import { getDaemonConfig } from "./config/daemonConfig";
 import { initHttpServer } from "./http/httpServer";
-import { ContainerWrapper } from "./container/container";
 
 async function init() {
     const daemonConfig = await getDaemonConfig();
@@ -24,36 +23,6 @@ async function init() {
     // TODO load this daemon's containers from the api
 
     await initHttpServer(logger);
-
-    const apps = await getApps();
-    let id = 0;
-    let hostPort = 20000;
-    for (const [appId, app] of Object.entries(apps)) {
-        if (!app.variants || Object.keys(app.variants).length === 0) {
-            continue;
-        }
-        for (const [variantId, variant] of Object.entries(app.variants)) {
-            if (!variant.versions || Object.keys(variant.versions).length === 0) {
-                continue;
-            }
-            for (const [versionId, version] of Object.entries(variant.versions)) {
-                ContainerWrapper.register(`${++id}`, {
-                    appId,
-                    variantId,
-                    versionId,
-                    dockerImage: version.default_docker_image,
-                    ports: [
-                        {
-                            containerPort: 25565,
-                            hostPort: hostPort++
-                        }
-                    ],
-                    segments: 3
-                });
-            }
-        }
-    }
-    logger.info(`Started ${id} servers`);
 }
 
 init().then(() => logger.info("Ready"));
