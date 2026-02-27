c3dd191ba1151e2a58522d6b0eb4a91fb11db78d
Dom <dominicraebeeson@gmail.com>
2026-02-27 17:37:04 +0000

    set user and group on install to get docker gid

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM node:24-alpine
+FROM node:24-bookworm-slim
 
-RUN apk -U update && apk upgrade && apk add 7zip
+RUN apt update --fix-missing && apt upgrade -y && apt install -y 7zip
 
-WORKDIR "/home/ogsh"
-RUN adduser --uid 1337 --disabled-password --gecos "" ogsh && chown -R 1337 /home/ogsh
-USER ogsh
-
+WORKDIR /ogsh
 ADD node_modules node_modules
 ADD build build
+RUN chown -R 1337:nogroup /ogsh
 
 ENTRYPOINT ["node", "build/daemon.js"]
--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 set -e
 
+# TODO ask user for app archives path
+# TODO ask user for container files path
+# startup files don't matter because they're small
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+DAEMON_BRANCH="main"
+
 # Validate API key to get Daemon ID
-OGSH_DAEMON_ID=""
-while [ "$OGSH_DAEMON_ID" = "" ]; do
+OGSH_DAEMON_ID="null"
+while [ "$OGSH_DAEMON_ID" = "null" ]; do
     read -p "Enter API key: " OGSH_DAEMON_API_KEY
-    OGSH_DAEMON_ID=$(curl -X GET https://api.opengameserverhost.com/v1/daemon/ -H "authorization: $OGSH_DAEMON_API_KEY")
+    json=$(curl -s -X GET https://api.opengameserverhost.com/v1/daemon/ -H "authorization: $OGSH_DAEMON_API_KEY")
+    OGSH_DAEMON_ID=$(jq -r .data.id <<< "$json")
+    if [ "$OGSH_DAEMON_ID" = "null" ]; then
+        printf "Invalid API key\n"
+    else
+        printf "Daemon ID: $OGSH_DAEMON_ID\n"
+    fi
 done
 
 # Install Docker
 function get_distro() {
-    if [[ -f /etc/os-release ]]
-    then
+    if [[ -f /etc/os-release ]]; then
         source /etc/os-release
         echo $ID_LIKE
     else
@@ -38,3 +54,13 @@ while [ ! -S "$OGSH_DOCKER_SOCK_PATH" ]; do
 done
 
 # TODO start on boot and automatically restart container on stop
+printf "\nCreating Daemon container\n"
+CONTAINER_TAG="ghcr.io/open-game-server-host/daemon:$DAEMON_BRANCH"
+docker rm -f ogsh_daemon # TODO silent if there is no container
+# TODO change credentials path from my local machine
+# TODO move api key to credentials file so it can be changed
+docker container create -it -u 1337:$(getent group docker | cut -d: -f3) --cpus=1 --memory=512m --restart unless-stopped -v /home/dom/Documents/open-game-server-host/Git/daemon/credentials.json:/ogsh/credentials.json -e OGSH_DAEMON_ID="$OGSH_DAEMON_ID" -e OGSH_DAEMON_API_KEY="$OGSH_DAEMON_API_KEY" -v $OGSH_DOCKER_SOCK_PATH:/var/run/docker.sock --name ogsh_daemon $CONTAINER_TAG
+printf "\nStarting Daemon container\n"
+docker start ogsh_daemon
+
+printf "Done\n"
