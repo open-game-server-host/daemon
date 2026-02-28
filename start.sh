09e8bb5c1abb061ceed94fa8b08dae6ac069cf98
Dom <dominicraebeeson@gmail.com>
2026-02-28 17:25:23 +0000

    don't overwrite docker.sock path after checking it

--- start.sh
+++ start.sh
@@ -19,15 +19,18 @@ if [ ! -f "$API_KEY_PATH" ]; then
 fi
 
 DOCKER_SOCK_PATH="$BASE_PATH/docker.sock"
-if [ ! -f "$DOCKER_SOCK_PATH" ]; then
-    printf "WARN  Docker socket not found! ($DOCKER_SOCK_PATH)\n"
-fi
-DOCKER_SOCK_PATH="/var/run/docker.sock"
-if [ ! -S "$DOCKER_SOCK_PATH" ]; then
-    printf "ERROR Docker socket not found! ($DOCKER_SOCK_PATH)\n"
-    exit 1
+if [ -S "$DOCKER_SOCK_PATH" ]; then
+    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH'\n"
+elif [ -S "/var/run/docker.sock" ]; then
+    DOCKER_SOCK_PATH="/var/run/docker.sock"
+    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH'\n"
 else
-    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH' instead\n"
+    if [ "$DOCKER_SOCK_PATH" = "/var/run/docker.sock" ]; then
+        printf "ERROR Could not find docker socket at '$DOCKER_SOCK_PATH'!\n"
+    else
+        printf "ERROR Could not find docker socket at '$DOCKER_SOCK_PATH' or '/var/run/docker.sock'!\n"
+    fi
+    exit 1
 fi
 
 CONTAINER_FILES_PATH="$BASE_PATH/container_files"
@@ -44,5 +47,5 @@ CONTAINER_TAG="ghcr.io/open-game-server-host/daemon:$BRANCH"
 printf "INFO  Pulling container image '$CONTAINER_TAG'\n"
 docker pull "$CONTAINER_TAG"
 docker rm -f $CONTAINER_NAME
-docker run -d -u $(id -u "$USER"):$(getent group docker | cut -d: -f3) --read-only --cpus=1 --memory=500m -v $API_KEY_PATH:/ogsh/apikey -v $DOCKER_SOCK_PATH:/var/run/docker.sock -v "$CONTAINER_FILES_PATH":/ogsh/container_files -v "$APP_ARCHIVES_PATH":/ogsh/app_archives -v "$STARTUP_FILES_PATH":/ogsh/startup_files --name $CONTAINER_NAME $CONTAINER_TAG
+docker run -d -u $(id -u "$USER"):$(getent group docker | cut -d: -f3) --read-only --cpus=1 --memory=500m -v $API_KEY_PATH:/ogsh/api_key -v $DOCKER_SOCK_PATH:/var/run/docker.sock -v "$CONTAINER_FILES_PATH":/ogsh/container_files -v "$APP_ARCHIVES_PATH":/ogsh/app_archives -v "$STARTUP_FILES_PATH":/ogsh/startup_files --name $CONTAINER_NAME $CONTAINER_TAG
 printf "INFO  Started container '$CONTAINER_NAME'\n"
