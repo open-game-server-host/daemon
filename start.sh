bc4c9ed2be931328aaf4bfed93e65502d4de6565
Dom <dominicraebeeson@gmail.com>
2026-03-07 23:09:41 +0000

    don't parse \n when printing

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+NL="\\n"
+
 BASE_PATH="$(realpath .)"
-printf "INFO  Working directory '$BASE_PATH'\n"
+printf "INFO  Working directory '$BASE_PATH'$NL"
 
 BRANCH=$1
 if [ -z "$BRANCH" ]; then
@@ -10,16 +12,16 @@ if [ -z "$BRANCH" ]; then
 fi
 
 USER="$(whoami)"
-printf "INFO  Running as user '$USER'\n"
+printf "INFO  Running as user '$USER'$NL"
 
-printf "INFO  Checking for new start script\n"
+printf "INFO  Checking for new start script$NL"
 START_SCRIPT_PATH="$BASE_PATH/start.sh"
 START_SCRIPT_URL="https://raw.githubusercontent.com/open-game-server-host/daemon/refs/heads/$BRANCH/start.sh"
 NEW_START_SCRIPT="$(curl $START_SCRIPT_URL)"
 if [ "$(cat $START_SCRIPT_PATH)" = "$NEW_START_SCRIPT" ]; then
-    printf "INFO  This is the latest version\n"
+    printf "INFO  This is the latest version$NL"
 else
-    printf "INFO  Restarting to update\n"
+    printf "INFO  Restarting to update$NL"
     sleep 3
     NEW_START_SCRIPT_PATH="$BASE_PATH/start.sh.update"
     printf "$NEW_START_SCRIPT" > "$NEW_START_SCRIPT_PATH"
@@ -30,46 +32,46 @@ fi
 
 API_KEY_PATH="$BASE_PATH/api_key"
 if [ ! -f "$API_KEY_PATH" ]; then
-    printf "ERROR API key not found! ($API_KEY_PATH)\n"
+    printf "ERROR API key not found! ($API_KEY_PATH)$NL"
     exit 1
 fi
 
 DOCKER_SOCK_PATH="$BASE_PATH/docker.sock"
 if [ -S "$DOCKER_SOCK_PATH" ]; then
-    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH'\n"
+    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH'$NL"
 elif [ -S "/var/run/docker.sock" ]; then
     DOCKER_SOCK_PATH="/var/run/docker.sock"
-    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH'\n"
+    printf "INFO  Using docker socket at '$DOCKER_SOCK_PATH'$NL"
 else
     if [ "$DOCKER_SOCK_PATH" = "/var/run/docker.sock" ]; then
-        printf "ERROR Could not find docker socket at '$DOCKER_SOCK_PATH'!\n"
+        printf "ERROR Could not find docker socket at '$DOCKER_SOCK_PATH'!$NL"
     else
-        printf "ERROR Could not find docker socket at '$DOCKER_SOCK_PATH' or '/var/run/docker.sock'!\n"
+        printf "ERROR Could not find docker socket at '$DOCKER_SOCK_PATH' or '/var/run/docker.sock'!$NL"
     fi
     exit 1
 fi
 
 APP_ARCHIVES_PATH="$(cat app_archives_path)"
 if [ -z "$APP_ARCHIVES_PATH" ]; then
-    printf "ERROR '$(realpath app_archives_path)' is empty!\n"
+    printf "ERROR '$(realpath app_archives_path)' is empty!$NL"
     exit 1
 fi
 CONTAINER_FILES_PATH="$(cat container_files_path)"
 if [ -z "$CONTAINER_FILES_PATH" ]; then
-    printf "ERROR '$(realpath container_files_path)' is empty!\n"
+    printf "ERROR '$(realpath container_files_path)' is empty!$NL"
     exit 1
 fi
 STARTUP_FILES_PATH="$(cat startup_files_path)"
 if [ -z "$STARTUP_FILES_PATH" ]; then
-    printf "ERROR '$(realpath startup_files_path)' is empty!\n"
+    printf "ERROR '$(realpath startup_files_path)' is empty!$NL"
     exit 1
 fi
 
 CONTAINER_NAME="ogsh_daemon"
 CONTAINER_TAG="ghcr.io/open-game-server-host/daemon:$BRANCH"
-printf "INFO  Pulling container image '$CONTAINER_TAG'\n"
+printf "INFO  Pulling container image '$CONTAINER_TAG'$NL"
 docker pull "$CONTAINER_TAG"
 docker rm -f $CONTAINER_NAME
 docker run -d -u $(id -u):$(getent group docker | cut -d: -f3) --read-only --cpus=1 --memory=500m -v $API_KEY_PATH:/ogsh/api_key -v $DOCKER_SOCK_PATH:/var/run/docker.sock -v "$CONTAINER_FILES_PATH":/ogsh/container_files -v "$APP_ARCHIVES_PATH":/ogsh/app_archives -v "$STARTUP_FILES_PATH":/ogsh/startup_files -e "HOST_CONTAINER_FILES_PATH=$CONTAINER_FILES_PATH" -e "HOST_STARTUP_FILES_PATH=$STARTUP_FILES_PATH" -e "CONTAINER_USERNAME=$USER" --name $CONTAINER_NAME $CONTAINER_TAG
-printf "INFO  Started container '$CONTAINER_NAME'\n"
+printf "INFO  Started container '$CONTAINER_NAME'$NL"
 docker logs -f $CONTAINER_NAME
