b6576a0b0ae81187e6e254be56252cc0e4224735
Dom <dominicraebeeson@gmail.com>
2026-03-08 00:41:54 +0000

    set group to uid

--- src/docker.ts
+++ src/docker.ts
@@ -91,7 +91,7 @@ export async function createDockerContainer(options: ContainerCreateOptions): Pr
 			name: options.name,
 			VolumeDriver: "local",
 			Env: parsedEnvVariables,
-			User: UID,
+			User: `${UID}:${UID}`,
 			HostConfig: {
 				Memory: getMb(options.memoryMb),
 				MemorySwap: getMb(options.memoryMb) + getMb(500),
